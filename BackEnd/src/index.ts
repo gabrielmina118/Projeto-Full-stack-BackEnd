547bf3edd820b9365ad914011e3bb4e6ca01d983
gabrielMina <gabrielmina118@gmail.com>
2021-06-30 16:10:14 -0300

    endpoint de criar TAGS , imagem e buscar imagens pela TAG

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
+import { imageRouter } from './routes/imageRouter';
 import { userRouter } from './routes/userRouter';
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/user", userRouter)
+app.use("/user", userRouter);
+app.use("/image",imageRouter)
 
 app.listen(3003, () => {
   console.log(`Servidor rodando em http://localhost:3003`);
