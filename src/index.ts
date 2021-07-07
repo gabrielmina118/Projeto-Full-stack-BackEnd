b63c294b84c67d5795ad0d84eb7f3afca68f21e7
gabrielMina <gabrielmina118@gmail.com>
2021-07-07 15:47:26 -0300

    subindo cors

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from 'express'
 import { followRouter } from './routes/followRouter';
 import { imageRouter } from './routes/imageRouter';
 import { userRouter } from './routes/userRouter';
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/user", userRouter);
 app.use("/image",imageRouter);
